ba7c22d5556b16399f6cdd554f891c9075d109b5
nickoff <nick.olehnovich@gmail.com>
2024-02-11 01:45:04 +0300

    feat: implememt mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,47 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.clearAllTimers();
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spyAxiosCreate = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    } as unknown as jest.Mocked<typeof axios>);
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+
+    await throttledGetDataFromApi('');
+    expect(spyAxiosCreate).toHaveBeenCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const spyAxiosCreate = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: {} }),
+    } as unknown as jest.Mocked<typeof axios>);
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const instance = axios.create({ baseURL });
+
+    await throttledGetDataFromApi('');
+    jest.runAllTimers();
+    expect(instance.get).toHaveBeenCalledWith('');
+    spyAxiosCreate.mockRestore();
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const spyAxiosCreate = jest.spyOn(axios, 'create').mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: 'test' }),
+    } as unknown as jest.Mocked<typeof axios>);
+    const response = await throttledGetDataFromApi('');
+
+    jest.runAllTimers();
+    expect(response).toEqual('test');
+    spyAxiosCreate.mockRestore();
   });
 });
