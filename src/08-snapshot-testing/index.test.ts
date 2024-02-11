b68618196798963ad84ac6ea5c30f85193a22f52
nickoff <nick.olehnovich@gmail.com>
2024-02-11 10:43:57 +0300

    feat: implememt snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,24 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const value1 = [null, 'one', 2];
+    const extendedValue1 = {
+      value: null,
+      next: {
+        value: 'one',
+        next: { value: 2, next: { value: null, next: null } },
+      },
+    };
+
+    expect(generateLinkedList(value1)).toStrictEqual(extendedValue1);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const value2 = [null, 'one', 2, undefined, null];
+
+    expect(generateLinkedList(value2)).toMatchInlineSnapshot();
   });
 });
